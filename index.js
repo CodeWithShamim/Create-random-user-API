63e65243e1bae93f90944e5ad413dd9261cbb29a
codewithshamim <shamimislamonline@gmail.com>
2022-09-02 17:33:02 +0600

    port updated

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require("cors");
 const userRoutes = require('./routes/v1/users.route.js');
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
